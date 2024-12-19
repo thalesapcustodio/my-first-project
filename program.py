e819d5fca7948260642312c24b3acbebb854e529
ThalesCustodio <tapc23professionalcontact@protonmail.com>
2024-12-19 16:58:35 -0300

    Test update.

---
+++ program.py
@@ -0,0 +1,36 @@
+#AULA OPERADORES E CONDICIONAIS
+numero_1 = 10
+numero_2 = 8
+
+
+resultado = numero_1 + -numero_2
+
+#print("O resultado é: {}".format(resultado))
+
+#idade = int(input("Digite a sua idade: "))
+#
+#if idade < 18:
+#    print("Menor de idade!")
+#elif idade == 18:
+#    print("Maior de idade com exatos 18 anos")
+#else:
+#    print("Maior de idade com mais de 18 anos")
+
+####################
+test = 0
+
+while True:
+   #print(test)
+   test += 1
+
+   if test % 2 == 0:
+      print("{} é par!".format(test))
+      continue
+
+   print(test, "Não é par.")
+
+
+
+# lista_de_compras = ["Ovos", "leite", "creatina", "whey protein"]
+# for item in lista_de_compras:
+#     print("Você comprou: {}".format(item))
